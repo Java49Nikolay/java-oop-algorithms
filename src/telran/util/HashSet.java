8c7ed3fd7d4a28f36a79d559ee4a860c83884571
Nik.choporev <nik.choporev@gmail.com>
2023-05-28 19:41:30 +0300

    CW #16 before break

--- src/telran/util/HashSet.java
+++ src/telran/util/HashSet.java
@@ -28,7 +28,8 @@ public class HashSet<T> implements Set<T> {
 		private int getCurrentIteratorIndex(int currentIndex) {
 			currentIndex++;
 			while(currentIndex < hashTable.length && 
-					(hashTable[currentIndex] == null || hashTable[currentIndex].size() == 0)) {
+					(hashTable[currentIndex] == null || 
+					hashTable[currentIndex].size() == 0)) {
 				currentIndex++;
 			}
 			return currentIndex < hashTable.length ? currentIndex : -1;
